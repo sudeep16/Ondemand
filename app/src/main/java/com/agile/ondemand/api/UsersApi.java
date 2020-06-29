e2a2766a45ebc281aa1abf17d38c6c96be5cc70c
sushantmzjn <[sushantmzjn@gmail.com]>
2020-06-29 12:59:10 +0545

    login

--- app/src/main/java/com/agile/ondemand/api/UsersApi.java
+++ app/src/main/java/com/agile/ondemand/api/UsersApi.java
@@ -5,11 +5,17 @@ import com.agile.ondemand.serverresponse.SignUpResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface UsersApi {
     /**User Registration*/
-
     @POST("users/register")
     Call<SignUpResponse> registerUser(@Body User user);
+
+    /**User Login*/
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("username") String username, @Field("password") String password);
 }
