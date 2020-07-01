302a82e1424f9fd0447d91db141c26fac017da8e
sushantmzjn <[sushantmzjn@gmail.com]>
2020-07-01 21:44:19 +0545

    carousel

--- app/src/main/java/com/agile/ondemand/fragments/HomeFragment.java
+++ app/src/main/java/com/agile/ondemand/fragments/HomeFragment.java
@@ -4,18 +4,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.agile.ondemand.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class HomeFragment extends Fragment {
+
+    private int[] images = new int[]{
+            R.drawable.a, R.drawable.b,
+            R.drawable.c, R.drawable.d
+    };
+
+    private String[] imageTitle = new String[]{
+            "x", "y", "z", "g"
+    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CarouselView carouselView = root.findViewById(R.id.carousel);
+        carouselView.setPageCount(images.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(images[position]);
+            }
+        });
+
+//        carouselView.setImageClickListener(new ImageClickListener() {
+//            @Override
+//            public void onClick(int position) {
+//                Toast.makeText(getActivity(), imageTitle[position] , Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        return root;
     }
 }
