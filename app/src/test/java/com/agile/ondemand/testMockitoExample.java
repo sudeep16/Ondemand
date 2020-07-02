0c98eefd55a2696b6aab93c96f88e2228841985d
Nischal Shakya <nischal.shk@gmail.com>
2020-07-02 11:07:48 +0545

    login registration and Api token unit test

--- app/src/test/java/com/agile/ondemand/testMockitoExample.java
+++ app/src/test/java/com/agile/ondemand/testMockitoExample.java
@@ -6,9 +6,9 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+//import org.mockito.junit.MockitoJUnitRunner;
 
-import static com.google.common.truth.Truth.assertThat;
+//import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
 
@@ -17,24 +17,24 @@ import static org.mockito.Mockito.when;
 public class testMockitoExample {
 
 
-
-        private static final String FAKE_STRING = "HELLO WORLD";
-
-        @Mock
-        Context mockContext;
-
-        @Test
-        public void readStringFromContext_LocalizedString() {
-            // Given a mocked Context injected into the object under test...
-            when(mockContext.getString(R.string.hello_world))
-                    .thenReturn(FAKE_STRING);
-            ClassUnderTest myObjectUnderTest = new ClassUnderTest(mockContext);
-
-            // ...when the string is returned from the object under test...
-            String result = myObjectUnderTest.getHelloWorldString();
-
-            // ...then the result should be the expected one.
-            assertThat(result, is(FAKE_STRING));
-        }
+//
+//        private static final String FAKE_STRING = "HELLO WORLD";
+//
+//        @Mock
+//        Context mockContext;
+//
+//        @Test
+//        public void readStringFromContext_LocalizedString() {
+//            // Given a mocked Context injected into the object under test...
+//            when(mockContext.getString(R.string.hello_world))
+//                    .thenReturn(FAKE_STRING);
+//            ClassUnderTest myObjectUnderTest = new ClassUnderTest(mockContext);
+//
+//            // ...when the string is returned from the object under test...
+//            String result = myObjectUnderTest.getHelloWorldString();
+//
+//            // ...then the result should be the expected one.
+//            assertThat(result, is(FAKE_STRING));
+//        }
     }
 
