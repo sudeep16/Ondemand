88c0376496d0d3c0da50308d597360d244b24a52
Nischal Shakya <nischal.shk@gmail.com>
2020-07-02 11:10:03 +0545

    cleanup.Work on validations & dashboard left

---
+++ app/src/test/java/com/agile/ondemand/testDashboard.java
@@ -0,0 +1,23 @@
+package com.agile.ondemand;
+
+import org.junit.Test;
+
+public class testDashboard {
+
+
+    @Test
+    public void testSortingCategory(){
+
+
+
+    }
+    @Test
+    public void testSortingJobs(){
+
+
+
+    }
+
+
+
+}
