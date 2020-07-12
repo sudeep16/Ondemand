c0feb8cbfa1fbf71b69a445e4b7f5d22663b819e
sushantmzjn <[sushantmzjn@gmail.com]>
2020-07-12 14:17:55 +0545

    logout

--- app/src/main/java/com/agile/ondemand/fragments/ProfileFragment.java
+++ app/src/main/java/com/agile/ondemand/fragments/ProfileFragment.java
@@ -1,21 +1,53 @@
 package com.agile.ondemand.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.agile.ondemand.R;
+import com.agile.ondemand.activity.LoginActivity;
 
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                logOut();
+            }
+        });
+
+
+        return view;
     }
+
+    private void logOut() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("User", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+
+        Intent intent = new Intent(getContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
